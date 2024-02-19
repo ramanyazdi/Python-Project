c2e8f4961aac05eabbe97402891473b751a51f27
RAHMAN <ramanyazdi@gmail.com>
2024-02-19 12:22:54 +0800

    first commit

---
+++ email_sender.py
@@ -0,0 +1,25 @@
+from email.message import EmailMessage
+from app2 import password
+import ssl
+import smtplib 
+
+email_sender = 'ramanyazdi@gmail.com'
+email_password = password
+email_receiver = "kadkhodarostamm@gmail.com"
+
+subject = 'my python email sender'
+body = """
+Hi. this the body of my email. 
+"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp: 
+    smtp.login(email_sender, email_password )
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
