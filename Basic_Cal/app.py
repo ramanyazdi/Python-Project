f319d5eb85c49a729122732b433259b4ab2f8cae
RAHMAN <ramanyazdi@gmail.com>
2024-02-22 20:43:33 +0800

    cal first function

---
+++ Basic_Cal/app.py
@@ -0,0 +1,13 @@
+
+
+choice = input("what do u want to do?")
+
+
+a = int(input('tell me 1st number'))
+b = int(input('tell me 2cnd number'))
+
+def add(a,b):
+    #result = int(a)+int(b)
+    print(a+b)
+
+add(a,b)
