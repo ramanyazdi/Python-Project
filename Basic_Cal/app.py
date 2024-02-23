676277a920b6f4fd2c7ea080e4de78b46c1b1aeb
RAHMAN <ramanyazdi@gmail.com>
2024-02-23 19:42:49 +0800

    creat basic cal

--- Basic_Cal/app.py
+++ Basic_Cal/app.py
@@ -1,13 +1,42 @@
 
-
-choice = input("what do u want to do?")
-
-
-a = int(input('tell me 1st number'))
-b = int(input('tell me 2cnd number'))
-
 def add(a,b):
-    #result = int(a)+int(b)
-    print(a+b)
+    print(str(a) + ' + ' + str(b) + ' = ' + str(a+b))
 
-add(a,b)
+def multiply (a,b):
+    print(str(a) + ' * ' + str(b) + ' = ' + str(a*b))
+
+def divide(a,b):
+    print(str(a) + ' / ' + str(b) + ' = ' + str(a/b))
+
+def deduct(a,b):
+    print(str(a) + ' - ' + str(b) + ' = ' + str(a-b))
+
+while True: 
+    text = ''' what do u want to do?
+    + = add
+    - = deduct 
+    * = multiply 
+    / = divide
+    . = exit
+    '''
+    choice = input(text)
+
+    if choice in ["+" , "-" , "*" , "/"]: 
+        a = int(input('tell me 1st number: '))
+        b = int(input('tell me 2cnd number: '))
+    
+        if choice == "+":
+            add(a,b)
+        elif choice == "*":
+            multiply(a,b) 
+        elif choice == "/":
+            divide(a,b)
+        elif choice == "-":
+            deduct(a,b) 
+    elif choice == ".":
+            print('Exit')
+            quit() 
+    else: 
+        print('Error: Enter right charactor')
+
+        
