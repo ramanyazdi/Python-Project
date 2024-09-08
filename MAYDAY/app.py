7441d75645938462bd603f0f65339cf425d40032
RAHMAN <ramanyazdi@gmail.com>
2024-09-08 11:44:52 +0330

    solution

--- MAYDAY/app.py
+++ MAYDAY/app.py
@@ -2,3 +2,20 @@ string = "Whiskey Hotel Four Tango Dash Alpha Romeo Three Dash Yankee Oscar Unif
 
 words = string.split(" ")
 
+dict = {"Zero" : 0, "One" : 1, "Two" : 2, "Three" : 3, "Four" : 4, 
+        "Five" : 5, "Six" : 6, "Seven" : 7, "Eight" : 8,
+        "Nine" : 9, "Dash" :"-"}
+
+# 1st solution
+
+for w in words:
+    if w in dict:
+        print(dict[w], end="")
+    else:
+        print(w[0], end="")
+
+# 2nd solution
+print()
+for w in words:
+    print(dict.get(w, w[0]), end='')
+
