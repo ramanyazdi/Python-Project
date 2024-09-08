7448319cc0ed69ef6d965c81e4000578ce7fd54b
RAHMAN <ramanyazdi@gmail.com>
2024-09-08 11:04:31 +0330

    Assign value to string and split it

---
+++ MAYDAY/app.py
@@ -0,0 +1,4 @@
+string = "Whiskey Hotel Four Tango Dash Alpha Romeo Three Dash Yankee Oscar Uniform Dash Sierra One November Kilo India November Golf Dash Four Bravo Zero Uniform Seven"
+
+words = string.split(" ")
+
